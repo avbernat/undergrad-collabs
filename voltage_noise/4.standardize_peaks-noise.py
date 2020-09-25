2239a108cafb8f52d742b577cceab41e35b446e1
Anastasia Bernat <anastasiabernat@Anastasias-MacBook-Pro.local>
2020-09-25 14:56:04 -0500

    answering questions on the standardization peak script

--- voltage_noise/4.standardize_peaks-noise.py
+++ voltage_noise/4.standardize_peaks-noise.py
@@ -1,29 +1,6 @@
 import statistics
-
-#************************************************************************************************************
-# This function spits out a list of file names in a given directory. The purpose is to loop through this list
-# so as to run this standardization over many files.
-#************************************************************************************************************
-
 import os
 
-# Autumn 2019
-#path = "/Users/anastasiabernat/Desktop/Flight trials_WinDAQ Recordings_Fall 2019/Text Files/"
-#path = "/Users/anastasiabernat/Desktop/odd_text_files/"
-
-
-username=os.getlogin() #package that talks to operating system
-#print(username)
-
-#Winter 2020
-path = f"/Users/{username}/Desktop/git_repositories/undergrad-collabs/voltage_noise/test_files/"
-
-#print(path)
-
-dir_list = sorted(os.listdir(path))
-
-print("Files and directories in", path, "' :")
-    
 #************************************************************************************************************
 # This function performs a standardization of the voltage data for each channel. This is achieved by defining a
 # **confidence interval** around the mean voltage value using a low (min_val) and high (max_val) threshold. These
@@ -35,7 +12,7 @@ print("Files and directories in", path, "' :")
 #************************************************************************************************************
 
 def peak_standardization(column):
-    format_column = [] # column of rounded values to two decimal places
+    format_column = [] # column of voltage values rounded to two decimal places
     new_list=[]
     peaks=[]
     
@@ -44,7 +21,6 @@ def peak_standardization(column):
    
     print(format_column[0:10])
 
-    print(format_column[0:10])
     # Threshold values can be modified accordingly.
     channel_mean = (sum(format_column)/len(format_column))
     min_val=round(channel_mean - 0.01, 2) # The default values are set to deliver a fine tune signal standardization
@@ -54,36 +30,47 @@ def peak_standardization(column):
     print("min value: ", min_val)
     print("max value: ", max_val)
 
-    for ii in range(0, len(format_column)):  
+    for v in range(0, len(format_column)):  
     #finding instances how voltage value compares to min (ie if less than min by a lot)
-        x=(format_column[ii]-min_val)/(max_val-min_val) 
-        #x says how far we are to min relative to how far the max is from the min. 
-        #Is there a statistical name for x?
-        if ii == 0 or ii == 1: #if x =1, max_val = min_val
+        x=(format_column[v]-min_val)/(max_val-min_val) 
+
+        #x says how far we are to min relative to how far the max is from the min.
+
+        # Is there a statistical name for x? AB: Yes! Good question, it's called the Minmax normalization
+        # or min-max algorithm. When the format_column value is = the max value, x = 1. This means, 
+        # the minimum value in X is mapped to 0 and the maximum value in X is mapped to 1. However, overall,
+        # we are transforming the data set from one range to a range to another (our voltages can be much
+        # much lower than the min value, leading to those high negative values).
+
+        if v == 0 or v == 1: 
             print("x:", x)
-            print("numerator:", (format_column[ii]-min_val))
+            print("numerator:", (format_column[v]-min_val))
             print("denominator:", (max_val-min_val))
 
+        # Peak vs. nonpeak threshold  
         if x < -2:  # used negative 2 a stronger cut off when determining what is a dip in voltage. 
         #If our voltage value twice as far below the min value as the min value is from the max value
-            new_list.append(1)  
+            new_list.append(1)  # define as peak
         else:
-            new_list.append(0) 
-#tells us when there is a really low value; how do all these zeroes and ones get understood as a peak
-        if ii == 0 or ii==1:
+            new_list.append(0) # not a peak
+        
+        #tells us when there is a really low value; how do all these zeroes and ones get understood as a peak
+
+        if v == 0 or v==1:
             print("x:", x)
-            print("numerator:",format_column[ii]-min_val)
+            print("numerator:",format_column[v]-min_val)
             print("denominator:",max_val-min_val)
 
+           
+    for ii in range(0, len(new_list)-1): 
+        # range(1, len(new_list)-1); when we evaluate for i=0, new_list[0 -1]= last element of new list
 
-            #range(1, len(new_list)-1); when we evaluate for i=0, new_list[0 -1]= last element of new list
-    for iii in range(0, len(new_list)-1): 
-        vals = f"iii = {iii}, new_list[{iii}] = {new_list[iii]}, new_list[{iii-1}] = {new_list[iii-1]}, new_list[{iii+1}] = {new_list[iii+1]}"
+        vals = f"iii = {ii}, new_list[{ii}] = {new_list[ii]}, new_list[{ii-1}] = {new_list[ii-1]}, new_list[{ii+1}] = {new_list[ii+1]}"
         print(vals) #tells us for each value the three things we compared to one another 
        
-        if new_list[iii] > new_list[iii-1] and new_list[iii] >= new_list[iii+1]: 
+        if new_list[ii] > new_list[ii-1] and new_list[ii] >= new_list[ii+1]: 
             peaks.append(1)
-            #why don't we start range at 1 and not include line 86?
+            #why don't we start range at 1 and not include line 86? AB: try running and see what happens if you remove the -1 
         else:
             peaks.append(0)
     peaks.append(0)  
@@ -91,13 +78,27 @@ def peak_standardization(column):
     return peaks 
 
 #new_list evaluates whether voltage vlaue is significant or is it very low relative to max to min range 
+
 #************************************************************************************************************
-# The flight data file can be called by either defining the complete filepath (for example c:\desktop\recordings
-# \filename.txt) or defining a default path in the section "write the path here" that will be automatically
+# The flight data file can be called by defining the complete file directory path or defining a default path in
+# the section "write the path here" (for example c:\desktop\recordings \filename.txt) that will be automatically
 # recalled each time the function is run. In the latter case the user will only need to type the name of
 # the .txt or .dat file to process when requested.
 #************************************************************************************************************
 
+# Autumn 2019
+#path = "/Users/anastasiabernat/Desktop/Flight trials_WinDAQ Recordings_Fall 2019/Text Files/"
+#path = "/Users/anastasiabernat/Desktop/odd_text_files/"
+
+#Winter 2020
+username=os.getlogin() # package that talks to operating system
+path = f"/Users/{username}/Desktop/git_repositories/undergrad-collabs/voltage_noise/test_files/"
+print(path)
+
+dir_list = sorted(os.listdir(path))
+
+print("Files and directories in", path, "' :")
+    
 for file in dir_list:
     if file.startswith("."):
         continue #ignore
