a3160a673f079d564036e47281d3ea769ffc6b1b
Anastasia Bernat <anastasiabernat@Anastasias-MacBook-Pro.local>
2020-09-25 15:00:31 -0500

    last minute changes

--- voltage_noise/4.standardize_peaks-noise.py
+++ voltage_noise/4.standardize_peaks-noise.py
@@ -65,7 +65,7 @@ def peak_standardization(column):
     for j in range(0, len(new_list)-1): 
         # range(1, len(new_list)-1); when we evaluate for i=0, new_list[0 -1]= last element of new list
 
-        vals = f"iii = {j}, new_list[{j}] = {new_list[j]}, new_list[{j-1}] = {new_list[j-1]}, new_list[{j+1}] = {new_list[j+1]}"
+        vals = f"j = {j}, new_list[{j}] = {new_list[j]}, new_list[{j-1}] = {new_list[j-1]}, new_list[{j+1}] = {new_list[j+1]}"
         print(vals) #tells us for each value the three things we compared to one another 
        
         if new_list[j] > new_list[j-1] and new_list[j] >= new_list[j+1]: 
