e4cc5c90eca0bf002428c666bbbf3745b15893ae
Anastasia Bernat <anastasiabernat@Anastasias-MacBook-Pro.local>
2020-12-11 12:44:16 -0600

    preparing for slurm batch

--- max_speed/batch.sh
+++ max_speed/batch.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=py_diagnostics
-#SBATCH --ntasks-per-node=5
-#SBATCH --nodes=8
+#SBATch --account=edu
+#SBATCH --job-name=diagnostics
+#SBATCH --ntasks-per-node=1
+#SBATCH --nodes=5
 #SBATCH --mem-per-cpu=9000M
-#SBATCH --time=01-9:00
+#SBATCH --time=11:00:00
 #SBATCH --mail-user=avbernat@uchicago.edu
 #SBATCH --mail-type=ALL
 #SBATCH --partition=broadwl
@@ -13,6 +14,6 @@ echo "Working directory is `pwd`"
 echo "Starting Python at `date`."
 
 module load python
-python3 /home/avbernat/Desktop/undergrad-collabs/max_speed/diagnostics.py
+python3 /home/avbernat/Desktop/undergrad-collabs/max_speed/diagnostics-AB.py
 
 echo "Finished Python at `date`."
